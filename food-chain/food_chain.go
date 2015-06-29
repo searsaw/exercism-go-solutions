6768e2a1d9e2ee480e977dede0067d21dcda3ef2
Alex Sears <alexwsears@gmail.com>
2015-06-28 20:42:34 -0400

    Completed 'Food Chain'

---
+++ food-chain/food_chain.go
@@ -0,0 +1,92 @@
+package foodchain
+
+import (
+	"bytes"
+)
+
+// Used by the tests
+const TestVersion = 1
+
+func Verse(verse int) string {
+	return song[verse]
+}
+
+func Verses(verses ...int) string {
+	var buffer bytes.Buffer
+
+	for i := 0; i < len(verses)-1; i++ {
+		buffer.WriteString(song[verses[i]])
+		buffer.WriteString("\n\n")
+	}
+
+	buffer.WriteString(song[verses[len(verses)-1]])
+
+	return buffer.String()
+}
+
+func Song() string {
+	var buffer bytes.Buffer
+
+	for i := 1; i < len(song)-1; i++ {
+		buffer.WriteString(song[i])
+		buffer.WriteString("\n\n")
+	}
+
+	buffer.WriteString(song[len(song)-1])
+
+	return buffer.String()
+}
+
+var song = []string{``,
+
+	`I know an old lady who swallowed a fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`,
+
+	`I know an old lady who swallowed a spider.
+It wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`,
+
+	`I know an old lady who swallowed a bird.
+How absurd to swallow a bird!
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`,
+
+	`I know an old lady who swallowed a cat.
+Imagine that, to swallow a cat!
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`,
+
+	`I know an old lady who swallowed a dog.
+What a hog, to swallow a dog!
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`,
+
+	`I know an old lady who swallowed a goat.
+Just opened her throat and swallowed a goat!
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`,
+
+	`I know an old lady who swallowed a cow.
+I don't know how she swallowed a cow!
+She swallowed the cow to catch the goat.
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`,
+
+	`I know an old lady who swallowed a horse.
+She's dead, of course!`,
+}
